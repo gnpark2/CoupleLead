b36cd0858d62f924d43dc9a3d3fed5df7d79d084
gnpark2 <gnpark2@gmail.com>
2026-09-10 14:39:28 +0900

    Prometheus, Grafana 추가

--- src/main/java/com/example/couplead/auth/security/SecurityConfig.java
+++ src/main/java/com/example/couplead/auth/security/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.example.couplead.auth.security;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,56 +20,55 @@ public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider) throws Exception {
-            http
+    SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider)
+            throws Exception {
+        http
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
-                .sessionManagement(session ->
-                    session.sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS
-                    )
-                )
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                    .dispatcherTypeMatchers(jakarta.servlet.DispatcherType.ERROR).permitAll()
-                    .requestMatchers(
-                        "/api/auth/**",
-                        "/api/test/**",
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/uploads/**",
-                        // jwt websocket 구현 전까지 임시 허용
-                        "/ws",
-                        "/ws/**",
-                        // static resource 허용
-                        "chat-test.html",
-                        "/",
-                        "/css/**",
-                        "/js/**",
-                        "/images/**",
-                        "webjars/**",
-                        "/favicon.ico"
-                    )
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-                );
-            return http.build();
-        }
-    
+                        .dispatcherTypeMatchers(jakarta.servlet.DispatcherType.ERROR).permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/api/test/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/uploads/**",
+                                // jwt websocket 구현 전까지 임시 허용
+                                "/ws",
+                                "/ws/**",
+
+                                // Actuator
+                                "/actuator/health",
+                                "/actuator/prometheus",
+                                // static resource 허용
+                                "chat-test.html",
+                                "/",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**",
+                                "webjars/**",
+                                "/favicon.ico")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        return http.build();
+    }
+
     @Bean
     DaoAuthenticationProvider authenticationProvider(
-        CustomUserDetailsService customUserDetailsService,
-        PasswordEncoder passwordEncoder
-    ) {
+            CustomUserDetailsService customUserDetailsService,
+            PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
         provider.setPasswordEncoder(passwordEncoder);
 
         return provider;
     }
-    
+
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
